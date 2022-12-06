8bcb0eedfb2d625ebea7fe66ed78118e3ef001e4
hassan-chaudhry <hc2843@nyu.edu>
2022-12-05 21:45:14 -0500

    added lines of code to insert/update database

--- demo/clouddatabase.py
+++ demo/clouddatabase.py
@@ -1,5 +1,6 @@
 from google.cloud.sql.connector import Connector
 import sqlalchemy
+from sqlalchemy import update
 
 # initialize parameters
 INSTANCE_CONNECTION_NAME = f"grocerybuddy-370504:us-central1:grocery-buddy"
@@ -18,11 +19,11 @@ def getconn():
         "pymysql",
         user=DB_USER,
         password=DB_PASS,
-        db=DB_NAME
+        db=DB_NAME,
     )
     return conn
 
-# create connection pool with 'creator' argument to our connection object function
+# create connection pool with 'creator' argument to connection object function
 pool = sqlalchemy.create_engine(
     "mysql+pymysql://",
     creator=getconn,
@@ -30,20 +31,31 @@ pool = sqlalchemy.create_engine(
 
 # connect to connection pool
 with pool.connect() as db_conn:
-  # create ratings table in our movies database
-  db_conn.execute(
-      "CREATE TABLE IF NOT EXISTS gb_database "
-      "( id SERIAL NOT NULL, store VARCHAR(255) NOT NULL, "
-      "product VARCHAR(255) NOT NULL, price FLOAT NOT NULL, "
-      "PRIMARY KEY (id));"
-  )
+  # create table in database
+  # db_conn.execute(
+  #     "CREATE TABLE IF NOT EXISTS gb_database" 
+  #     "( id SERIAL NOT NULL, store VARCHAR(255) NOT NULL, "
+  #     "product VARCHAR(255) NOT NULL, price FLOAT NOT NULL, "
+  #     "PRIMARY KEY (id));"
+  #)
 
- # insert data into our ratings table
-  insert_stmt = sqlalchemy.text(
+  # get table object
+  metaData = sqlalchemy.MetaData()
+  gb_database = sqlalchemy.Table('gb_database', metaData, autoload=True, autoload_with=pool)
+
+  # insert data into table
+  insert_database = sqlalchemy.text(
       "INSERT INTO gb_database (store, product, price) VALUES (:store, :product, :price)",
   )
 
- # query and fetch ratings table
+  # to insert new data: stores, products, and prices [EXAMPLE BELOW: insert "Organic Large Brown Eggs, 24 oz" for "5.79"]
+  #db_conn.execute(insert_database, store="wholefoods, product="Organic Large Brown Eggs, 24 oz", price="5.79")
+
+  # to update data [EXAMPLE BELOW: update price for "Organic Large Hass Avocados" to "5.00"]
+  # query = sqlalchemy.update(gb_database).values(price="5.00").where(gb_database.columns.product=="Organic Large Hass Avocados") 
+  # db_conn.execute(query)
+
+  # get table results
   results = db_conn.execute("SELECT * FROM gb_database").fetchall()
 
   # show results
