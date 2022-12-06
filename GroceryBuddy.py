3bc4f714a1246bf6d33d93463c753d6094a2bcb4
hassan-chaudhry <hc2843@nyu.edu>
2022-12-06 00:13:25 -0500

    intergrated cloud database into main program + cleaned up code

--- GroceryBuddy.py
+++ GroceryBuddy.py
@@ -1,66 +1,103 @@
-from tkinter import Widget
-import kivy
-import pandas as pd
+#######################
+#                     # 
+#    GROCERY BUDDY    #
+#                     #
+#######################
 
+##############
+#  MODULES   #
+##############
+
+#  kivy modules for GUI
+import kivy
 from kivy.app import App
+from kivymd.app import MDApp
+from kivy.lang import Builder
+from kivy.lang.builder import Builder
+
 from kivy.uix.label import Label
 from kivy.uix.button import Button
 from kivy.uix.textinput import TextInput
 from kivy.uix.gridlayout import GridLayout
-from kivy.app import App
-from kivy.lang import Builder
+
+from kivy.core.window import Window
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.scrollview import ScrollView
 from kivy.properties import StringProperty, ObjectProperty, NumericProperty
-from kivy.lang import Builder
-from kivymd.app import MDApp
 from kivymd.uix.datatables import MDDataTable
-from kivy.core.window import Window
-
-from kivy.lang.builder import Builder
-from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.metrics import dp
 
+# panda module for data tables
+from tkinter import Widget
+import pandas as pd
+
+# SQL and Google Cloud to connect to database
+import sqlalchemy
+from google.cloud.sql.connector import Connector
+
+# PIL and pytesseract modules for image to text conversion
 from PIL import Image
 from pytesseract import pytesseract
 
-import requests
-from bs4 import BeautifulSoup
+############################
+#  CLOUD DATABASE ACCESS   #
+############################
 
-# store products
-wholefoods_products = {"Organic Honeycrisp Apple": "4.45", "Organic Large Hass Avocados": "5.00",
-                       "Large Hass Avocados": "4.00", "Organic Broccoli": "2.99", "Medium Hass Avocado": "0.99",
-                       "Honeycrisp Apples": "3.29", "Organic Blueberries Pint": "5.99",
-                       "Organic Green Asparagus": "4.39", "Organic Fuji Apples": "2.99", "Organic Raspberries": "7.99",
-                       "Organic Grade A Whole Milk, 1 gallon": "6.99", "Full Fat Oat Milk, 64 fl oz": "5.99",
-                       "Organic Large Brown Eggs, 24 oz": "5.79", "Large Eggs, 36 oz": "8.79"}
-walmart_products = {"Lightly Dried Organic Parsley": "0.35", "Organic Bananas": "1.42",
-                    "Organic Baby Peeled Carrots": "1.56", "Organic Grape Tomato": "2.66",
-                    "Organic Bagged Avocados": "4.98", "Fresh Organic Mini Cucumbers": "3.46",
-                    "Organic Baby Spinach": "2.98", "Organic Spring Mix": "4.98", "Envy Apples": "1.36",
-                    "Gala Apples": "0.84", "a2 Milk Whole Milk": "3.97",
-                    "Great Value Whole Vitamin D Milk, Gallon, 128 fl oz": "$4.37",
-                    "Deans TruMoo 1% Low Fat Chocolate Milk, Gallon, 128 fl oz": "5.37",
-                    "Great Value Large White Eggs, 12 Count": "2.82", "Great Value Large White Eggs, 18 Count": "4.12"}
+# initialize parameters
+INSTANCE_CONNECTION_NAME = f"grocerybuddy-370504:us-central1:grocery-buddy"
+DB_USER = "GroceryBuddy"
+DB_PASS = "GroceryBuddy7!"
+DB_NAME = "grocery_buddy_database"
 
+# initialize Connector object
+connector = Connector()
+
+# function to return the database connection object
+def getconn():
+    conn = connector.connect(
+        INSTANCE_CONNECTION_NAME,
+        "pymysql",
+        user=DB_USER,
+        password=DB_PASS,
+        db=DB_NAME
+    )
+    return conn
+
+# create connection pool with 'creator' argument to connection object function
+pool = sqlalchemy.create_engine(
+    "mysql+pymysql://",
+    creator=getconn,
+)
+
+# connect to connection pool
+with pool.connect() as db_conn:
+    insert_database = sqlalchemy.text("INSERT INTO gb_database (store, product, price) VALUES (:store, :product, :price)"), # use insert_stmt to insert data into our database
+    
+    # database table layout: id = store[0], stores = database[1], products = database[2], prices = database[3]
+    database = db_conn.execute("SELECT * FROM gb_database").fetchall()
+
+    # divide table into stores
+    wholefoods_products = db_conn.execute("SELECT * FROM gb_database WHERE store='whole_foods'").fetchall()
+    walmart_products = db_conn.execute("SELECT * FROM gb_database WHERE store='walmart'").fetchall()
+
+##############
+#  SCREENS   #
+##############
 
 class MainWidget(Screen):
     pass
 
-
 class SecondWindow(Screen):
     pass
-
 
 class WindowManager(ScreenManager):
     pass
 
-
 class ThirdWindow(Screen):
     pass
 
-
 class FourthWindow(Screen):
+# search for product in all stores
     def pressSBP(self):
         user_product = self.ids.userInputSBP.text  # product that user searches for
 
@@ -71,23 +108,25 @@ class FourthWindow(Screen):
         displayNPFWal = True  # if no products found in Walmart
 
         for product in wholefoods_products:  # Whole Foods Products
-            if (user_product.lower() in product.lower()):
+            store_product = str(product[2])
+            if (user_product.lower() in store_product.lower()):
                 displayNPFWF = False
                 if (displayWFHeader == True):
                     productInStore += "Whole Foods Products: \n"
                     displayWFHeader = False
-
-                productInStore += product + ", $" + wholefoods_products.get(product) + "\n"
+                product_price = str(product[3])
+                productInStore += store_product + ", $" + product_price + "\n"
         productInStore += "\n"
 
         for product in walmart_products:  # Walmart Products
-            if (user_product.lower() in product.lower()):
+            store_product = str(product[2])
+            if (user_product.lower() in store_product.lower()):
                 displayNPFWal = False
                 if (displayWalHeader == True):
                     productInStore += "Walmart Products: \n"
                     displayWalHeader = False
-
-                productInStore += product + ", $" + walmart_products.get(product) + "\n"
+                product_price = str(product[3])
+                productInStore += store_product + ", $" + product_price + "\n"
         productInStore += "\n"
 
         if (displayNPFWF == True and displayNPFWal == True):
@@ -97,17 +136,15 @@ class FourthWindow(Screen):
 
     pass
 
-
 class FifthWindow(Screen):
     pass
-
 
 class ScrollLabel(ScrollView):
     text = StringProperty("\n")
     pass
 
-
 class SixthWindow(Screen):
+# search whole foods for product
     def pressWF(self):
         # scroll view properties
         pd.set_option('display.max_rows', None)
@@ -123,7 +160,7 @@ class SixthWindow(Screen):
 
         # go through each item and append to stock
         for product in wholefoods_products:
-            stockWF.append(product)
+            stockWF.append(str(product))
 
         # adding the stock to the dataframe
         wholefoodsDF["Items"] = stockWF  # add items to dataframe
@@ -131,16 +168,22 @@ class SixthWindow(Screen):
         # check if item in stock
         user_product = self.ids.userInputWF.text
 
+        # get price of product 
+        for product in wholefoods_products:  
+            store_product = str(product[2])
+            store_price = str(product[3])
+            if store_product.lower() == user_product.lower():
+                break
+
         if user_product.lower() in stockWF.lower():
-            self.ids.productInWFStock.text = user_product + " are available at Whole Foods! The product is priced at $" + wholefoods_products.get(
-                user_product) + "."
+            self.ids.productInWFStock.text = user_product + " are available at Whole Foods! The product is priced at $" + store_price + "."
         else:
             self.ids.productInWFStock.text = "Our records indicate that " + user_product + " are currently unavailable at Whole Foods."
 
     pass
 
-
 class SeventhWindow(Screen):
+# search for walmart for product 
     def pressWal(self):
         # scroll view properties
         pd.set_option('display.max_rows', None)
@@ -156,7 +199,7 @@ class SeventhWindow(Screen):
 
         # go through each item and append to stock
         for product in walmart_products:
-            stockWal.append(product)
+            stockWal.append(str(product))
 
         # adding the stock to the dataframe
         walmartDF["Items"] = stockWal  # add items to dataframe
@@ -165,18 +208,30 @@ class SeventhWindow(Screen):
         # check if item in stock
         user_product = self.ids.userInputWal.text
 
+        # get price of product 
+        for product in wholefoods_products:  
+            store_product = str(product[2])
+            store_price = str(product[3])
+            if store_product.lower() == user_product.lower():
+                break
+
         if user_product.lower() in stockWal.lower():
-            self.ids.productInWalStock.text = user_product + " are available at Walmart! The product is priced at $" + walmart_products.get(
-                user_product) + "."
+            self.ids.productInWalStock.text = user_product + " are available at Walmart! The product is priced at $" + store_price + "."
         else:
             self.ids.productInWalStock.text = "Our records indicate that " + user_product + " are currently unavailable at Walmart."
 
     pass
 
-
 class EighthWindow(Screen):
+# data table for walmart products
     def add_datatable(self):
-        resultList = list(walmart_products.items())
+        wal_products = [] # list of products at walmart
+        wal_prices = [] # list of product prices at walmart
+        for product in walmart_products:
+            wal_products.append(str(product[2]))
+            wal_prices.append(str(product[3]))
+
+        resultList = (wal_products)
         table = MDDataTable(
             use_pagination=True,
             size_hint=(1, .5),
@@ -189,11 +244,17 @@ class EighthWindow(Screen):
         self.add_widget(table)
 
     pass
-
 
 class NinthWindow(Screen):
+# data table for whole foods products
     def add_datatable(self):
-        resultList = list(wholefoods_products.items())
+        wf_products = [] # list of products at whole foods
+        wf_prices = [] # list of product prices at whole foods
+        for product in wholefoods_products:
+            wf_products.append(str(product[2]))
+            wf_prices.append(str(product[3]))
+
+        resultList = (wf_products)
         table = MDDataTable(
             use_pagination=True,
             size_hint=(1, .5),
@@ -207,14 +268,13 @@ class NinthWindow(Screen):
 
     pass
 
-
 class TenthWindow(Screen):
+# drag & drop receipt
     filePath = StringProperty('')
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         Window.bind(on_dropfile=self._on_file_drop)
-
 
     def reduced_image(self): # fit on screen 
         print(self.filePath)
@@ -224,10 +284,11 @@ class TenthWindow(Screen):
         self.filePath = file_path.decode("utf-8") # read file path
         self.ids.receipt.source = self.filePath
         self.ids.receipt.reload() # reload screen with image
+
     pass
 
-
 class EleventhWindow(Screen):
+# read data from receipt
     def pressReceipt(self):
         filePath = self.manager.get_screen("AddReceipt").ids.receipt.source # get file path from TenthWindow
         img = Image.open(filePath)
@@ -236,6 +297,7 @@ class EleventhWindow(Screen):
     pass
 
 class TwelfthWindow(Screen):
+# add item to grocery list
     itemname_text_input = ObjectProperty()
     ego = NumericProperty(0)
     itemname = StringProperty('')
@@ -253,19 +315,21 @@ class TwelfthWindow(Screen):
     pass
 
 class ViewMyList(Screen):
+# read item from gorcery list
     def updateMyList(self):
         with open("itemname.txt", "r") as fobj:
             self.ids.itemlistlabel.text = fobj.read()
     pass
 
-kv = Builder.load_file("my.kv")
+######################
+#  BUILD KIVY FILE   #
+######################
 
+kv = Builder.load_file("my.kv")
 
 class MyMainApp(MDApp):
     def build(self):
         return kv
-
-
 
 if __name__ == "__main__":
     MyMainApp().run()
