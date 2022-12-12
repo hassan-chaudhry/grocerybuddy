afeb6e71847567aab92575a9cb0eeac00a6ea15d
hassan-chaudhry <hc2843@nyu.edu>
2022-12-11 22:30:54 -0500

    allows devs to access database

--- demo/clouddatabase.py
+++ demo/clouddatabase.py
@@ -1,12 +1,14 @@
 from google.cloud.sql.connector import Connector
 import sqlalchemy
+from sqlalchemy import insert
 from sqlalchemy import update
+from sqlalchemy import delete
 
 # initialize parameters
 INSTANCE_CONNECTION_NAME = f"grocerybuddy-370504:us-central1:grocery-buddy"
 print(f"Your instance connection name is: {INSTANCE_CONNECTION_NAME}")
 DB_USER = "GroceryBuddy"
-DB_PASS = "GroceryBuddy7!"
+DB_PASS = "GroceryBuddyPass7"
 DB_NAME = "grocery_buddy_database"
 
 # initialize Connector object
@@ -43,21 +45,29 @@ with pool.connect() as db_conn:
   metaData = sqlalchemy.MetaData()
   gb_database = sqlalchemy.Table('gb_database', metaData, autoload=True, autoload_with=pool)
 
-  # insert data into table
-  insert_database = sqlalchemy.text(
-      "INSERT INTO gb_database (store, product, price) VALUES (:store, :product, :price)",
-  )
-
-  # to insert new data: stores, products, and prices [EXAMPLE BELOW: insert "Organic Large Brown Eggs, 24 oz" for "5.79"]
-  #db_conn.execute(insert_database, store="wholefoods, product="Organic Large Brown Eggs, 24 oz", price="5.79")
-
-  # to update data [EXAMPLE BELOW: update price for "Organic Large Hass Avocados" to "5.00"]
-  # query = sqlalchemy.update(gb_database).values(price="5.00").where(gb_database.columns.product=="Organic Large Hass Avocados") 
+  # insert data into table - OLD
+  # query = sqlalchemy.insert(gb_database).values(store="Whole Foods Market at 1095 6th Ave, New York, NY 10036, USA", product="SALTED CORN CHIPS", price="2.59")
   # db_conn.execute(query)
 
-  # get table results
+  # to update data [EXAMPLE BELOW: update price for "Organic Large Hass Avocados" to "5.00"]
+  # query = sqlalchemy.update(gb_database).values(store="Walmart at 500 Bayonne Crossing Way, Bayonne, NJ 07002, USA").where(gb_database.columns.store=="Walmart") 
+  # db_conn.execute(query)
+
+  # get specific row results
+  # wholefoodsproducts = db_conn.execute("SELECT * FROM gb_database WHERE store='whole_foods'").fetchall()
+
+  # get specific coloum results 
+  # products = db_conn.execute("SELECT product FROM gb_database").fetchall()
+  # for prod in products:
+  # 	 print(prod)
+
+  # to delete data
+  # query = sqlalchemy.delete(gb_database).where(gb_database.c.id == 97)
+  # db_conn.execute(query)
+
+  # get entire table results
   results = db_conn.execute("SELECT * FROM gb_database").fetchall()
 
   # show results
   for row in results:
-    print(row)
+        print(row)
